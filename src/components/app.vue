6d15d43e84fa9333fb79c252f913b949247c05f7
massibull <massibull@gmail.com>
2025-07-24 23:22:59 +0200

    Create app.vue

---
+++ src/components/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Card title="Carta di Attacco" description="Attacca con 5 danni." />
+    <Card title="Carta di Difesa" description="Difende 3 danni." />
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue';
+
+export default {
+  components: {
+    Card,
+  };
+};
+</script>
+
+<style>
+/* Stili globali per l'app */
+</style>
